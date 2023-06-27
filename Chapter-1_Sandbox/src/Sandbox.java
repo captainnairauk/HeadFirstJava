c95d63ab05e3b030552c4ce73781f3cda947f821
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-06-27 13:05:06 +0530

    Create Sandbox Chapter

---
+++ Chapter-1_Sandbox/src/Sandbox.java
@@ -0,0 +1,9 @@
+public class Sandbox {
+    public static void main(String[] args) {
+        String [] roll = new String[4];
+        System.out.println(roll[0]);
+
+        int[] age = new int[10];
+        System.out.println(age[1]);
+    }
+}
